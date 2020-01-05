4025f044f6aec009bdf4f68ec00b98983271dc46
Thiago Lima <thiagolima.unb@gmail.com>
2020-01-05 15:59:15 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const PORT = 3000;
+app.listen(process.env.PORT || PORT, console.log(`Listening on port ${PORT}`));
